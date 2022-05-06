fcef8786e234240d17aa23c7af5447bdc0528a67
automaticp <automaticpte@gmail.com>
2022-05-06 16:06:10 +0300

    added insertion methods to TList

--- TList.m
+++ TList.m
@@ -36,10 +36,44 @@ classdef TList < handle
 			
 			obj.list(indecies) = [];
 			
-		end
+        end
+        
+        function insert_before(obj, this, inserted)
+            arguments
+                obj TList
+                this (1,1) TNode
+                inserted (1,:) TNode
+            end
+            pos = obj.pre_insert(this, inserted);
+            obj.list = [obj.list(1:pos-1), inserted, obj.list(pos:end)];
+        end
 		
-	end
-	
-	
+        function insert_after(obj, this, inserted)
+            arguments
+                obj TList
+                this (1,1) TNode
+                inserted (1,:) TNode
+            end
+            pos = obj.pre_insert(this, inserted);
+            obj.list = [obj.list(1:pos), inserted, obj.list(pos+1:end)];
+        end
+    end
+    
+	methods (Access = protected)
+        function pos = pre_insert(obj, this, inserted)
+            % Validates insertion arguments and returns the position of 'this' node
+            assert(numel(unique(inserted)) == numel(inserted), ...
+                'Duplicate nodes in the inserted list.')
+            
+            assert(~any(ismember(obj.list, inserted)), ...
+                'TList already contains some of the specified nodes.');
+           
+            index = obj.list == this; 
+            assert(any(index), 'TList has no specified node to insert before.'); 
+            
+            pos = find(index);
+
+        end
+    end
 end
 
